e8dd35aa30aabd8c0061ae4e6ad199bb44d540aa
Soket1 <76789444+Soket1@users.noreply.github.com>
2023-06-02 02:18:26 +0400

    Update BingAIClient.js

--- src/BingAIClient.js
+++ src/BingAIClient.js
@@ -120,9 +120,11 @@ export default class BingAIClient {
          uri: '23.26.126.113:2821', 
          token: `Basic ${Buffer.from('user117629:21tlbv').toString('base64')}`  
          });
-
+        console.log('Создан объект proxyAgent:'); 
+        console.log(proxyAgent);
         fetchOptions.dispatcher = proxyAgent;
-        console.log(JSON.stringify(proxyAgent, null, 2))
+        console.log('После использования proxyAgent:');
+        console.log(proxyAgent); 
        // }
         //const proxy = 'http://user117629:21tlbv@23.26.126.113:2821';
         //fetchOptions.proxy = proxy;
